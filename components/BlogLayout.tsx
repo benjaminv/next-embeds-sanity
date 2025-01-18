83ce9e2318f8259c00f20c1dee2ac8b8d75482d6
benjaminv <benvee@gmail.com>
2025-01-18 20:09:25 +1100

    add footer

--- components/BlogLayout.tsx
+++ components/BlogLayout.tsx
@@ -1,5 +1,5 @@
 import AlertBanner from 'components/AlertBanner'
-
+import Footer from 'components/Footer'
 export default function BlogLayout({
   preview,
   loading,
@@ -14,6 +14,7 @@ export default function BlogLayout({
       <div className="min-h-screen">
         <AlertBanner preview={preview} loading={loading} />
         <main>{children}</main>
+        <Footer />
       </div>
     </>
   )
