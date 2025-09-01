8778fdfa28e0f5098cc211745ea45050db838276
benjaminv <benvee@gmail.com>
2025-09-01 10:06:01 +1000

    setup: consolidated project to use newer sanity schema offered by sanity-io/sanity-template-nextjs-clean

--- sanity.config.ts
+++ sanity.config.ts
@@ -20,6 +20,7 @@ import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash'
 import authorType from 'schemas/author'
 import postType from 'schemas/post'
 import settingsType from 'schemas/settings'
+import blockContentType from 'schemas/objects/blockContent'
 
 const title =
   process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || 'Next.js Blog with Sanity.io'
@@ -31,7 +32,7 @@ export default defineConfig({
   title,
   schema: {
     // If you want more content types, you can add them to this array
-    types: [authorType, postType, settingsType],
+    types: [authorType, postType, settingsType, blockContentType],
   },
   plugins: [
     structureTool({
