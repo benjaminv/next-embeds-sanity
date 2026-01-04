0b2f1e5384521e922b1c4e9e8144cc6d43d326fb
benjaminv <benvee@gmail.com>
2026-01-04 21:21:33 +1100

    reverted back to align with upstream

--- intro-template/index.tsx
+++ intro-template/index.tsx
@@ -29,13 +29,8 @@ export default function IntroTemplate() {
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_GIT_PROVIDER}.com/${process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER}/${process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG}/blob/main/README.md#how-can-i-remove-the-next-steps-block-from-my-blog`
     : `https://github.com/sanity-io/nextjs-blog-cms-sanity-v3#how-can-i-remove-the-next-steps-block-from-my-blog`
 
-  // Return null during server-side rendering and initial client render to avoid hydration mismatch
-  if (!mounted) {
-    return null
-  }
-
   if (hasUTMtags || !studioURL) {
-    return null
+    return
   }
 
   return (
